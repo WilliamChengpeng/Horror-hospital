5cab0854804448f0ba254fc1704668aef9627938
Victormaa <389796272@qq.com>
2025-11-12 13:41:49 +0800

    upload

---
+++ Assets/Scripts/InteractCamera.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class InteractCamera : MonoBehaviour, IInteract
+{
+    public string showText;
+    public string Description()
+    {
+        return showText;
+    }
+
+    public void OnInteract()
+    {
+        //throw new System.NotImplementedException();
+        this.gameObject.SetActive ( false);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
