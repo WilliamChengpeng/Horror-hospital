ade4caadfc4546a604f5feaccd9e5e50776b1304
WilliamChengpeng <cheng.peng@northeastern.edu>
2025-11-11 20:04:02 -0800

    xiugai

--- Assets/Scripts/PVController.cs
+++ Assets/Scripts/PVController.cs
@@ -4,6 +4,11 @@ public class PVController : MonoBehaviour
 {
     public Animator room1Door;
     public Animator room2Door;
+    public Animator room3Door;
+    public Animator room4Door;
+    public Animator room5Door;
+    public Animator room6Door;
+    public Animator room7Door;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +18,15 @@ public class PVController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             room1Door.Play("Open");
             room2Door.Play("Open");
+            room3Door.Play("Open");
+            room4Door.Play("Open");
+            room5Door.Play("Open");
+            room6Door.Play("Open");
+            room7Door.Play("Open");
         }
 
     }
